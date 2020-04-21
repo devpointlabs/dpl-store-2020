6d5c6f61de1ce55d488d8cf0862aa2152e55f50b
Brianna Bell <bbell9563@gmail.com>
2020-04-21 16:01:40 -0600

    working responsive footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,45 +4,62 @@ import styled from 'styled-components';
 import Logo from '../images/dpl_logo.svg';
 import Pin from '../images/Pin.svg';
 
-const Footer = () => (
-  <div>
-    <StyledFooter as={Segment} inverted className="dpl-blue" style={{ marginBottom: '2px', paddingTop: '4%', paddingBottom: '3%',  borderRadius: '30px' }}>
-      <div style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
-        <div style={{ fontSize: '2vw', marginBottom: '7%' }}>Any questions?</div>
-        <div style={{ fontSize: '4vw', fontWeight: 'bold' }}>We're here to help.</div>
+class Footer extends React.Component {
+  state={isMobile: window.innerWidth < 900}
+  
+  componentDidMount(){
+    window.addEventListener('resize', this.handleResize)
+  }
 
+  handleResize = () => {
+    this.setState({isMobile: window.innerWidth < 900 })
+  }
+
+  render(){
+    const {isMobile} = this.state
+    return(<div>
+      <StyledFooter as={Segment} inverted className="dpl-blue" style={{ marginBottom: '2px', paddingTop: '2%', paddingBottom: '2%',  borderRadius: '30px' }}>
+        <div style={{display:'flex', justifyContent:'center', flexDirection:'column'}}>
+          <div style={isMobile ? { fontSize: '3vw', marginBottom: '7%' }: { fontSize: '2vw', marginBottom: '7%' }}>Any questions?</div>
+          <div style={isMobile ? { fontSize: '5vw', marginBottom: '7%' }: { fontSize: '4vw', fontWeight: 'bold' }}>We're here to help.</div>
+  
+        </div>
+        <div style={isMobile ? { width: '50%' } :{ width: '30%' }}>
+          <StyledContact style={isMobile ? {padding:'2%'} : {padding:'5%'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '1%', marginRight: '4%' }}>
+              <Icon name="mail" style={{ fontSize: '16px' }} />
+            </div>
+            <div style={isMobile ? { fontSize: '2vw' }: { fontSize: '1.5vw' }}>contact@devpointlabs.com</div>
+          </StyledContact>
+          <StyledContact style={isMobile ? {padding:'2%'} : {padding:'5%'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: '4%' }}>
+              <Image src={Pin} size="mini" inline style={{ height: "20px", width: "20px" }} />
+            </div>
+            <div >
+              <div style={isMobile ? { fontSize: '2vw' }: { fontSize: '1.5vw' }}>
+                370 South 300 East<br />
+              Salt Lake City, Utah<br />
+              84111</div>
+            </div>
+          </StyledContact>
+          <StyledContact style={isMobile ? {padding:'2%'} : {padding:'5%'}}>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '1%', marginRight: '4%' }}>
+              <Icon name="phone" flipped='horizontally' style={{ fontSize: '16px' }} />
+            </div>
+            <div style={isMobile ? { fontSize: '2vw' }: { fontSize: '1.5vw' }}>801-448-7420</div>
+          </StyledContact>
+        </div>
+      </StyledFooter>
+      <div className="dpl-blue" align="right" style={{ padding: '.5%', color: 'white' }}>
+        <p style={isMobile ? {fontSize:'2vw'}: {fontSize:'1vw'}}>
+          <Image align="left" size={isMobile ? 'tiny' : 'small'} src={Logo} className="filter-white"/>
+          2020 DevPoint Labs Terms Policy
+          </p>
       </div>
-      <div style={{ width: '30%' }}>
-        <StyledContact>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '1%', marginRight: '4%' }}>
-            <Icon name="mail" style={{ fontSize: '16px' }} />
-          </div>
-          <div style={{ fontSize: '1.5vw' }}>contact@devpointlabs.com</div>
-        </StyledContact>
-        <StyledContact>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginRight: '4%' }}>
-            <Image src={Pin} size="mini" inline style={{ height: "20px", width: "20px" }} />
-          </div>
-          <div >
-            <div style={{ fontSize: '1.5vw' }}>
-              370 South 300 East<br />
-            Salt Lake City, Utah<br />
-            84111</div>
-          </div>
-        </StyledContact>
-        <StyledContact>
-          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginLeft: '1%', marginRight: '4%' }}>
-            <Icon name="phone" flipped='horizontally' style={{ fontSize: '16px' }} />
-          </div>
-          <div style={{ fontSize: '1.5vw' }}>801-448-7420</div>
-        </StyledContact>
-      </div>
-    </StyledFooter>
-    <div className="dpl-blue" align="right" style={{ padding: '.5%', color: 'white' }}>
-      <p><Image align="left" size="small" src={Logo} className="filter-white"></Image>2020 DevPoint Labs Terms Policy</p>
-    </div>
-  </div>
-)
+    </div>)
+  }
+  
+}
 
 
 const StyledFooter = styled.div` {
@@ -85,7 +102,7 @@ const StyledContact = styled.div` {
   color:black;
   border-radius: 10px;
   font-size: 1vw;
-  margin: 3%;
+  margin: 2%;
   display:flex;
 }
 `
